ef7314cc70282accf84124f521262b85e19820fe
Consti <constantindietrich@mount7.com>
2023-10-27 00:32:58 +0200

    Add rough layout to the repo

---
+++ SystemDesignProject.h
@@ -0,0 +1,17 @@
+// declare all the functions here 
+
+#ifndef SYTEMDESIGNPROJECT_H_
+#define SYTEMDESIGNPROJECT_H_
+
+// include all the needed libarys via #include
+
+class NerdleGame {
+    public:
+        // The main function to run the robot.
+        void runRobot();
+    
+    private:
+        //declare all the private functions
+};
+
+#endif  // SYTEMDESIGNPROJECT_H_
